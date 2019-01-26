9c160daeca9f9a6e74095848acc0eaed1d365006
PizzaTopping <tomkattouw@hotmail.com>
2019-01-26 01:47:43 +0100

    Added head (camera) movement

--- GGJ2019/Assets/2_Scripts/Player/HandMover.cs
+++ GGJ2019/Assets/2_Scripts/Player/HandMover.cs
@@ -19,21 +19,21 @@ public class HandMover : MonoBehaviour
 
 	void Move()
 	{
-		float speed = _speed * Time.deltaTime;
+		float deltaSpeed = _speed * Time.deltaTime;
 
 		// Move forward and backward
 		float forwardInput = Input.GetAxis("Horizontal1");
-		_rigidbody.AddForce(Vector3.right * forwardInput * speed);
+		_rigidbody.AddForce(Vector3.right * forwardInput * deltaSpeed);
 		
 		// Move left and right
 		float sidewaysInput = Input.GetAxis("Horizontal2");
-		_rigidbody.AddForce(Vector3.forward * sidewaysInput * speed);
+		_rigidbody.AddForce(Vector3.forward * sidewaysInput * deltaSpeed);
 
 		// Move up 
 		if (Input.GetAxisRaw("LeftTrigger") < 0)
-			_rigidbody.AddForce(Vector3.up * speed);
+			_rigidbody.AddForce(Vector3.up * deltaSpeed);
 		else if (Input.GetKey(KeyCode.Joystick1Button4))
-			_rigidbody.AddForce(Vector3.down * speed);
+			_rigidbody.AddForce(Vector3.down * deltaSpeed);
 
 			// TODO set a max velocity here if needed
 	}
