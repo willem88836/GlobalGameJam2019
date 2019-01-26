f570f89701549b835ede2f57155dd46bec56de64
Justin Geschiere <j.geschiere@8d-games.nl>
2019-01-26 03:59:37 +0100

    Custom UI

---
+++ GGJ2019/Assets/2_Scripts/Networking/CustomNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CustomNetworkManager : NetworkManager
+{
+    [SerializeField] Button _serverButton;
+
+	[SerializeField] Button _joinButton;
+
+	[SerializeField] InputField _ipInput;
+
+	private void Awake()
+	{
+		_serverButton.onClick.AddListener(delegate { ServerPressed(); });
+		_joinButton.onClick.AddListener(delegate { JoinPressed(); });
+		_ipInput.onValueChanged.AddListener(delegate { IpChanged(_ipInput.text); });
+
+		IpChanged(_ipInput.text);
+	}
+
+	void ServerPressed()
+	{
+		StartServer();
+	}
+
+	void JoinPressed()
+	{
+		StartClient();
+	}
+
+	void IpChanged(string text)
+	{
+		networkAddress = text;
+	}
+}
