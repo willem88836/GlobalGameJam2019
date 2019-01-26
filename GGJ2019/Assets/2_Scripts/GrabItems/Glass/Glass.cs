c6d8418cc5932479cd69b89c6b9894f0cd527d8b
PizzaTopping <tomkattouw@hotmail.com>
2019-01-27 00:32:24 +0100

    glass spills water, still needs tweaking

---
+++ GGJ2019/Assets/2_Scripts/GrabItems/Glass/Glass.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Glass : GrabObject
+{
+	[SerializeField] GameObject _waterdropPrefab;
+
+	Transform _spilLocation;
+
+
+
+	float _spillTimer;
+	float _spillDuration = 0.2f;
+	
+	public override void Start()
+	{
+		base.Start();
+
+		_spilLocation = transform.GetChild(0);
+	}
+
+	void Update()
+	{
+		if (Grabbed)
+		{
+			SpilWater();
+		}
+	}
+
+	void SpilWater()
+	{
+		_spillTimer -= Time.deltaTime;
+		if (_spillTimer <= 0)
+		{
+			Instantiate(_waterdropPrefab, _spilLocation.position, Quaternion.identity);
+			_spillTimer = _spillDuration;
+		}
+	}
+}
