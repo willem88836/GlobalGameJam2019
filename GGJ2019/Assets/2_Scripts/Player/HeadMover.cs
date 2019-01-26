2d25f58dec9c3d25bbd1e9340436819a27de83a4
PizzaTopping <tomkattouw@hotmail.com>
2019-01-26 04:59:48 +0100

    Set limits to head rotation

--- GGJ2019/Assets/2_Scripts/Player/HeadMover.cs
+++ GGJ2019/Assets/2_Scripts/Player/HeadMover.cs
@@ -2,21 +2,44 @@
 
 public class HeadMover : MonoBehaviour
 {
+	Vector3 rotation = new Vector3();
+
 	[SerializeField] float _speed;
 
-    void Update()
+	[SerializeField] Vector2 _minBoundaries;
+	[SerializeField] Vector2 _maxBoundaries;
+
+	void Update()
     {
-		Move();
+		Rotate();
     }
 
-	void Move()
+	void Rotate()
 	{
 		float deltaSpeed = _speed * Time.deltaTime;
 
 		float horizontalInput = Input.GetAxis("HeadHorizontal");
-		transform.Rotate(Vector3.down * deltaSpeed * horizontalInput, Space.World);
+		//transform.Rotate(Vector3.down * deltaSpeed * horizontalInput, Space.World);
+		rotation.y -= deltaSpeed * horizontalInput;
 
 		float verticalInput = Input.GetAxis("HeadVertical");
-		transform.Rotate(Vector3.left * deltaSpeed * verticalInput, Space.Self);
+		//transform.Rotate(Vector3.left * deltaSpeed * verticalInput, Space.Self);
+		rotation.x -= deltaSpeed * verticalInput;
+		transform.localEulerAngles = rotation;
+		
+		RotationBoundaries();
+		transform.localEulerAngles = rotation;
+	}
+
+	void RotationBoundaries()
+	{
+		rotation = new Vector3(
+			Mathf.Clamp(rotation.x,
+			_minBoundaries.x,
+			_maxBoundaries.x),
+			Mathf.Clamp(rotation.y,
+			_minBoundaries.y,
+			_maxBoundaries.y)
+			);
 	}
 }
