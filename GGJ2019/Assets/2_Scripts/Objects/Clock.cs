e16e227f94f9fda5c6b73ec2a0d20265caba40e5
PizzaTopping <tomkattouw@hotmail.com>
2019-01-27 13:22:19 +0100

    clock has a script now

---
+++ GGJ2019/Assets/2_Scripts/Objects/Clock.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+	[SerializeField] Text _timerText;
+
+	float _timeLeft = 0;
+
+	bool _isCountingDown = false;
+
+	void Start()
+	{
+		_timerText.text = Mathf.Ceil(_timeLeft).ToString();
+	}
+
+	void Update()
+    {
+		if (_isCountingDown)
+			CountDown();
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			SetTimer(5, true);
+    }
+
+	public void SetTimer(float time, bool startCountingDown)
+	{
+		_timeLeft = time;
+
+		if (startCountingDown)
+			ToggleCountdown(true);
+	}
+
+	public void ToggleCountdown(bool isCountingDown)
+	{
+		_isCountingDown = isCountingDown;
+	}
+
+	void CountDown()
+	{
+		_timeLeft -= Time.deltaTime;
+		_timerText.text = Mathf.Ceil(_timeLeft).ToString();
+
+		if (_timeLeft <= 0)
+		{
+			_timeLeft = 0;
+			_isCountingDown = false;
+			FinishedCountdown();
+		}	
+	}
+
+	void FinishedCountdown()
+	{
+		// does nothing atm, but might be functional
+	}
+}
