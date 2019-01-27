4e69d43b821ca3671166374a22df3dd2fea915de
PizzaTopping <tomkattouw@hotmail.com>
2019-01-27 01:15:26 +0100

    water spill everywhere

--- GGJ2019/Assets/2_Scripts/GrabItems/Glass/Glass.cs
+++ GGJ2019/Assets/2_Scripts/GrabItems/Glass/Glass.cs
@@ -6,21 +6,26 @@ public class Glass : GrabObject
 
 	Transform _spilLocation;
 
-
+	Quaternion _defaultRotation;
 
 	float _spillTimer;
 	float _spillDuration = 0.2f;
-	
+
+	float _releaseTimer = 0;
+	float _releaseDuration = 2;
+
 	public override void Start()
 	{
 		base.Start();
 
 		_spilLocation = transform.GetChild(0);
+
+		_defaultRotation = transform.rotation;
 	}
 
 	void Update()
 	{
-		if (Grabbed)
+		if (Grabbed || _releaseTimer > 0)
 		{
 			SpilWater();
 		}
@@ -33,6 +38,14 @@ public class Glass : GrabObject
 		{
 			Instantiate(_waterdropPrefab, _spilLocation.position, Quaternion.identity);
 			_spillTimer = _spillDuration;
+
+			// Keep going for a while after not being grabbed
+			_releaseTimer -= Time.deltaTime;
+
+			Debug.Log(_releaseTimer);
+
+			if (Grabbed)
+				_releaseTimer = _releaseDuration;
 		}
 	}
 }
