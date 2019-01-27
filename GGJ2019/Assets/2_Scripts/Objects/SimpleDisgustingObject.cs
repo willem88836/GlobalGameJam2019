938f30aff4770ba280e5cd9013152851a9e20752
Justin Geschiere <j.geschiere@8d-games.nl>
2019-01-27 13:26:57 +0100

    Error fixes

--- GGJ2019/Assets/2_Scripts/Objects/SimpleDisgustingObject.cs
+++ GGJ2019/Assets/2_Scripts/Objects/SimpleDisgustingObject.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using UnityEngine;
 
-public class SimpleDisgustingObject : GrabObject, IDisgusting, IEdible, IObjective, IForkable
+public class SimpleDisgustingObject : GrabObject, IDisgusting, IEdible, IObjective
 {
 	[SerializeField] private float _disgustingValue = 5;
 
 	public Action<IObjective> OnComplete { get; set; }
 
 	public int Type { get; set; }
-	public int Player { get; set; }
+	public NetworkPlayer Player { get; set; }
 	public bool IsOnPlate { get; set; }
 
 	public virtual float GetDistgustingValue()
@@ -16,7 +16,7 @@ public class SimpleDisgustingObject : GrabObject, IDisgusting, IEdible, IObjecti
 		return _disgustingValue;
 	}
 
-	public void OnEat(Eater eater)
+	public void OnEat()
 	{
 		if (OnComplete != null)
 			OnComplete.Invoke(this);
